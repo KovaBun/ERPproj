284fcc541c5b5443342c9a6c117473697fb9b8a5
vasuiyer <grvasuiyer@gmail.com>
2026-03-14 18:13:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,17 +4,17 @@ import sqlite3 as sl3
 class Database:
     def __init__(self):
        self.con=sl3.Connection("/home/venkat/Documents/AppDB") 
-    def insertUser(self, username, password):
+    def insertUser(self, username, password,public_key):
         try:
-            sql="insert into users(username,password) values(?,?)"
-            self.con.execute(sql,(username,password))
+            sql="insert into users(username,password) values(?,?,?)"
+            self.con.execute(sql,(username,password,public_key))
             self.con.commit()
             return True
         except sl3.IntegrityError:
             return False
-    def getUser(self,username,password):
-        sql="select 1 from users where username=? AND password=?"
-        cursor=self.con.execute(sql,(username,password))
+    def getUser(self,username,password,public_key):
+        sql="select 1 from users where username=? AND password=? AND public_key=?"
+        cursor=self.con.execute(sql,(username,password,public_key))
         return cursor.fetchone()
                 
         
